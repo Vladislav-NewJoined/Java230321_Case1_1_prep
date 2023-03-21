3c03ce4eabd658f41decdf5aa5aea28158358057
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-21 23:02:49 +0300

    Initial commit

--- src/case_1_1.java
+++ src/case_1_1.java
@@ -2,7 +2,8 @@ import java.io.IOException;
 
 public class case_1_1 {
     // Задание 1. Кейс «Анализатор курса валют».
-    // инфо здесь: видео: Урок6 Модуль1 мин 44.30.
+    // инфо здесь: Урок6 Модуль1 мин 44.30  ( https://lms.synergy.ru/learning/view/68488/?groupPeriodId=1045153&disciplineVersionId=25858&start=1 )
+    // https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=13/11/2021&VAL_NM_RQ=R01235
     public static void main(String[] args) throws IOException {
 
     }
