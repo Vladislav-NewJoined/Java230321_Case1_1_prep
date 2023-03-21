0e121daa29b9b4bb1e66cda20b89403a9aa2eef2
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-21 22:28:16 +0300

    Initial commit

--- src/case_1_1.java
+++ src/case_1_1.java
@@ -1,5 +1,9 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.IOException;
+
+public class case_1_1 {
+    // Задание 1. Кейс «Анализатор курса валют».
+    // инфо здесь: видео: Урок6 Модуль1 мин 44.30.
+    public static void main(String[] args) throws IOException {
+
     }
 }
