a9fc6f5071fd9a4f4691f3459bc98b6948216d1b
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-27 13:32:01 +0300

    Initial commit

--- src/Case_1_1_1.java
+++ src/Case_1_1_1.java
@@ -4,34 +4,145 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Case_1_1_1 {
     // Задание 1. Кейс «Анализатор курса валют».
+    // Пользователь вводит дату.
+    // 1. Произвести анализ курса валют на эту дату, предыдущую и следующую:
+    // - Вывести, все три  курса
+    // - насколько курс вырос/упал.
+    // - Наибольший и наименьшие значения из этих трех
+    // - Сохранить в отдельную директорию лучший снимок NASA за эту дату :)
+
     // инфо здесь: Урок6 Модуль1 мин 44.30  ( https://lms.synergy.ru/learning/view/68488/?groupPeriodId=1045153&disciplineVersionId=25858&start=1 )
     // https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=13/11/2021&VAL_NM_RQ=R01235
-    public static void main(String[] args) throws IOException {
-        String page1 = downloadWebPage("https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=13/11/2021&VAL_NM_RQ=R01235");
-        int startIndex1 = page1.lastIndexOf("<Value>");
-        int endIndex1 = page1.lastIndexOf("</Value>");
-        String courseStr1 = page1.substring(startIndex1 + 7, endIndex1);
+    public static void main(String[] args) throws IOException, ParseException {
+        String originalString = downloadWebPage("https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=12/11/2021&VAL_NM_RQ=R01235");
 
-        String page2 = downloadWebPage("https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2020&date_req2=13/11/2020&VAL_NM_RQ=R01235");
-        int startIndex2 = page2.lastIndexOf("<Value>");
-        int endIndex2 = page2.lastIndexOf("</Value>");
-        String courseStr2 = page2.substring(startIndex2 + 7, endIndex2);
+        BufferedReader buffered = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(courseStr1);
-        System.out.println(courseStr2);
+        System.out.println("Введите исходную дату с разделителем '/': пример: 14/02/2020");
+        String originalDate = buffered.readLine();  // Start date
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar c = Calendar.getInstance();
+        c.setTime(sdf.parse(originalDate));
+        c.add(Calendar.DATE, -1);  // number of days to add
+        String oneDayBeforeDate;
+        oneDayBeforeDate = sdf.format(c.getTime());  // entering oneDayBeforeDate
+        c.add(Calendar.DATE, 2);  // number of days to add
+        String oneDayAfterDate;
+        oneDayAfterDate = sdf.format(c.getTime());  // entering oneDayAfterDate
 
-        double course1 = Double.parseDouble(courseStr1.replace(",", "."));
-        double course2 = Double.parseDouble(courseStr2.replace(",", "."));
+//        System.out.println(originalDate);
+//        System.out.println(oneDayBeforeDate);
+//        System.out.println(oneDayAfterDate);
 
-        if (course1 > course2) {
-            System.out.print("Курс подрос на ");
-            System.out.print(course1 - course2);
+        // Задаём адрес исходной веб-страницы Центробанка в текстовом формате.
+//        System.out.println("Исходная страница:");
+        String originalStrText = "https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=12/11/2021&VAL_NM_RQ=R01235";
+//        System.out.println(originalStrText + System.lineSeparator());
+
+        // Меняем в адресе исходной страницы даты на введённые (в трех строках).
+//        System.out.println("Страницы после ввода даты:");
+        String urlWithDate1 = originalStrText.replaceAll("12/11/2021", originalDate);
+//        System.out.println(urlWithDate1);
+        String urlWithDate2 = originalStrText.replaceAll("12/11/2021", oneDayBeforeDate);
+//        System.out.println(urlWithDate2);
+        String urlWithDate3 = originalStrText.replaceAll("12/11/2021", oneDayAfterDate);
+//        System.out.println(urlWithDate3);
+//        System.out.println();
+
+        String page1 = downloadWebPage(urlWithDate1);
+        String page2 = downloadWebPage(urlWithDate2);
+        String page3 = downloadWebPage(urlWithDate3);
+
+        // Задаём курсы с типом переменной double.
+        double courseDoble1 = 0;
+        double courseDoble2 = 0;
+        double courseDoble3 = 0;
+
+        if (page1.contains("<Value>")) {
+            int startIndex1 = page1.lastIndexOf("<Value>");
+            int endIndex1 = page1.lastIndexOf("</Value>");
+            String courseStr1 = page1.substring(startIndex1 + 7, endIndex1);
+            double course1 = Double.parseDouble(courseStr1.replace(",", "."));
+            courseDoble1 = course1;
+        }
+        if (page2.contains("<Value>")) {
+            int startIndex2 = page2.lastIndexOf("<Value>");
+            int endIndex2 = page2.lastIndexOf("</Value>");
+            String courseStr2 = page2.substring(startIndex2 + 7, endIndex2);
+            double course2 = Double.parseDouble(courseStr2.replace(",", "."));
+            courseDoble2 = course2;
+        }
+        if (page3.contains("<Value>")) {
+            int startIndex3 = page3.lastIndexOf("<Value>");
+            int endIndex3 = page3.lastIndexOf("</Value>");
+            String courseStr3 = page3.substring(startIndex3 + 7, endIndex3);
+            double course3 = Double.parseDouble(courseStr3.replace(",", "."));
+            courseDoble3 = course3;
+        }
+
+        System.out.println();
+
+        String noCurse = "На указанную дату курс отсутствует";
+        if (courseDoble1 == 0) {
+            System.out.println("Курс на " + originalDate + ": " + noCurse);
         } else {
-            System.out.print("Год назад курс был больше на ");
-            System.out.print(course2 - course1);
+            System.out.println("Курс на " + originalDate + ": " + courseDoble1);
+        }
+
+        if (courseDoble2 == 0) {
+            System.out.println("Курс на " + oneDayBeforeDate + ": " + noCurse);
+        } else {
+            System.out.println("Курс на " + oneDayBeforeDate + ": " + courseDoble2);
+        }
+
+        if (courseDoble3 == 0) {
+            System.out.println("Курс на " + oneDayAfterDate + ": " + noCurse);
+        } else {
+            System.out.println("Курс на " + oneDayAfterDate + ": " + courseDoble3);
+        }
+
+        System.out.println();
+
+        if (courseDoble1 == 0 || courseDoble2 == 0 || courseDoble3 == 0) {
+            System.out.println("Имеются даты с отсутствующим курсом. Повторите программу и введите другую дату.");
+        } else {
+            if (courseDoble3 > courseDoble2) {
+                System.out.print("Курс вырос на ");
+                System.out.println(courseDoble3 - courseDoble2 + "\n");
+            } else {
+                if (courseDoble3 < courseDoble2) {
+                    System.out.print("Курс снизился на ");
+                    System.out.println(courseDoble2 - courseDoble3 + "\n");
+                }
+            }
+            boolean courseDoble1Max = courseDoble1 > courseDoble2 && courseDoble1 > courseDoble3;
+            boolean courseDoble2Max = courseDoble2 > courseDoble1 && courseDoble2 > courseDoble3;
+            if (courseDoble1Max) {
+                System.out.println("Максимальный курс: " + courseDoble1 + "; " + "Приходится на дату: " + originalDate);
+            } else {
+                if (courseDoble2Max) {
+                    System.out.println("Максимальный курс: " + courseDoble2 + "; " + "Приходится на дату: " + oneDayBeforeDate);
+                } else {
+                    System.out.println("Максимальный курс: " + courseDoble3 + "; " + "Приходится на дату: " + oneDayAfterDate);
+                }
+            }
+            boolean courseDoble1Min = courseDoble1 < courseDoble2 && courseDoble1 < courseDoble3;
+            boolean courseDoble2Min = courseDoble2 < courseDoble1 && courseDoble2 < courseDoble3;
+            if (courseDoble1Min) {
+                System.out.println("Минимальный курс: " + courseDoble1 + "; " + "Приходится на дату: " + originalDate);
+            } else {
+                if (courseDoble2Min) {
+                    System.out.println("Минимальный курс: " + courseDoble2 + "; " + "Приходится на дату: " + oneDayBeforeDate);
+                } else {
+                    System.out.println("Минимальный курс: " + courseDoble3 + "; " + "Приходится на дату: " + oneDayAfterDate);
+                }
+            }
         }
     }
 
---
+++ src/Case_1_1_1_Draft_8_VarVneCykla.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Case_1_1_1_Draft_8_VarVneCykla {
+    // Задание 1. Кейс «Анализатор курса валют».
+    // инфо здесь: Урок6 Модуль1 мин 44.30  ( https://lms.synergy.ru/learning/view/68488/?groupPeriodId=1045153&disciplineVersionId=25858&start=1 )
+    // https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=13/11/2021&VAL_NM_RQ=R01235
+    public static void main(String[] args) throws IOException, ParseException {
+        int c = 0;
+        for(int i=566; i<567; i++)
+        {
+            c=c+i;
+        }
+        int d=c;
+        System.out.println(d);
+
+    }
+}
