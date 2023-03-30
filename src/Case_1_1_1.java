30e21384af20e93be58156fdb3289b5a16a7be57
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-03-30 09:26:14 +0300

    Initial commit

--- src/Case_1_1_1.java
+++ src/Case_1_1_1.java
@@ -4,6 +4,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -144,7 +146,28 @@ public class Case_1_1_1 {
                 }
             }
         }
+
+        String pageNasa = downloadWebPage("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY");
+        int urlBegin = pageNasa.lastIndexOf("url");
+        int urlEnd = pageNasa.lastIndexOf("}");
+        String urlPhoto = pageNasa.substring(urlBegin + 6, urlEnd - 1);
+        try(InputStream in = new URL(urlPhoto).openStream()){
+            Files.copy(in, Paths.get("new.jpg"));
+        }
+
+        System.out.println("\n" + "Картинка сохранена!");
+
+        int explanationBegin = pageNasa.lastIndexOf("explanation");
+        int explanationEnd = pageNasa.lastIndexOf("hdurl");
+        String explanation = pageNasa.substring(explanationBegin + 14, explanationEnd - 3);
+        System.out.println(explanation);
+
+
+
     }
+
+
+
 
     private static String downloadWebPage(String url) throws IOException {
         StringBuilder result = new StringBuilder();
