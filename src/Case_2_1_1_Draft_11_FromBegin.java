c0ab025a47f20c571550610cf2013da706df4d98
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-04-14 20:59:53 +0300

    Initial commit

--- src/Case_2_1_1_Draft_11_FromBegin.java
+++ src/Case_2_1_1_Draft_11_FromBegin.java
@@ -4,8 +4,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -14,11 +12,29 @@ public class Case_2_1_1_Draft_11_FromBegin {
     // Кейс «Анализатор курса валют».
     // Задание 2. - Пользователь вводит месяц и год. Вывести курс рубля за этот месяц, найти наибольший и наименьшие значения
     // Инфо здесь: Как найти анализ курса валют за определенную дату. Урок 6 Видео мин 0.44.56
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException {
+        String originalPage = downloadWebPage("https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=12/11/2021&VAL_NM_RQ=R01235");
+        // Задаём адрес исходной веб-страницы Центробанка в текстовом формате.
+        String originalStrText = "https://cbr.ru/scripts/XML_dynamic.asp?date_req1=12/11/2021&date_req2=12/11/2021&VAL_NM_RQ=R01235";
 
 
+        int startIndex = originalPage.lastIndexOf("<Value>") + 7;
+        int endIndex = originalPage.lastIndexOf("</Value>");
+        System.out.println(originalPage.substring(startIndex, endIndex));
 
 
+    }
 
+    private static String downloadWebPage(String url) throws IOException {
+        StringBuilder result = new StringBuilder();
+        String line;
+        URLConnection urlConnection = new URL(url).openConnection();
+        try (InputStream is = urlConnection.getInputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            while ((line = br.readLine()) != null) {
+                result.append(line);
+            }
+        }
+        return result.toString();
     }
 }
